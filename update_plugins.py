669d5924d4b31c580ac87e04f01160045ae3f713
ghassheee <ghassheee@gmail.com>
2017-10-09 12:11:40 +0900

    Updated

--- update_plugins.py
+++ update_plugins.py
@@ -39,6 +39,7 @@ vim-snippets https://github.com/honza/vim-snippets
 vim-surround https://github.com/tpope/vim-surround
 vim-expand-region https://github.com/terryma/vim-expand-region
 vim-multiple-cursors https://github.com/terryma/vim-multiple-cursors
+vim-solidity https://github.com/tomlion/vim-solidity
 vim-fugitive https://github.com/tpope/vim-fugitive
 goyo.vim https://github.com/junegunn/goyo.vim
 vim-zenroom2 https://github.com/amix/vim-zenroom2
